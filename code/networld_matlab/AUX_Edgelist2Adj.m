91e8c6f655db510fc866b2257d8d6298b297d6ce
Marcos Martinez <marcos.martinezj@estudiante.uam.es>
2023-05-19 12:26:11 +0200

    Corrections and results

--- code/networld_matlab/AUX_Edgelist2Adj.m
+++ code/networld_matlab/AUX_Edgelist2Adj.m
@@ -1,11 +1,17 @@
 function adj_mat = AUX_Edgelist2Adj(edgelist)
 %AUX_EDGELIST2ADJ Summary of this function goes here
 %   Detailed explanation goes here
-n = max(edgelist(:));
-adj_mat = zeros(n);
-for i=1:length(edgelist)
-    adj_mat(edgelist(i,1),edgelist(i,2)) = 1;
+if length(edgelist) == 0
+    adj_mat = [0];
+else
+    n = max(edgelist(:));
+    adj_mat = zeros(n);
+    [n_links, ~] = size(edgelist);
+    for i=1:n_links
+        adj_mat(edgelist(i,1),edgelist(i,2)) = 1;
+    end
+    adj_mat = adj_mat + transpose(adj_mat);
 end
-adj_mat = adj_mat + transpose(adj_mat);
+
 end
 
