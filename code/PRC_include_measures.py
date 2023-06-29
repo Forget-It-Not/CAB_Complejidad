37a7e4fa35dbd676c45d55cefa1937949b253e9d
Marcos Martinez <marcos.martinezj@estudiante.uam.es>
2023-06-29 08:53:41 +0200

    tonteria

--- code/PRC_include_measures.py
+++ code/PRC_include_measures.py
@@ -3,14 +3,17 @@ import pandas as pd
 
 data_path = 'data/Networld_N200_TMax10000/'
 
-betas = [0.001]
+betas = [0.001,0.0015849,0.0025119,0.0039811,0.0063096,
+         0.01,0.015849,0.025119,0.039811,0.063096,
+         0.1,0.15849,0.25119,0.39811,0.63096,
+         1, 1.5849, 2.5119, 3.9811, 6.3096]
 
 measures = pd.read_csv('data/metadata_measures.csv')
 ### This in case the column isnt already included in the file, but id rather
 ### do it just once...
-#measures.reset_index(names='NRed', inplace=True)
-#measures['NRed'] = measures['NRed'] + 1
-#measures.to_csv('data/metadata_measures.csv', index=False)
+# measures.reset_index(names='NRed', inplace=True)
+# measures['NRed'] = measures['NRed'] + 1
+# measures.to_csv('data/metadata_measures.csv', index=False)
 
 
 for beta in betas:
